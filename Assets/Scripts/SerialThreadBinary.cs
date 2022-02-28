de5b9637eeda8620c1c9082e9df7471c222183a9
Ana <63330374+aenikka@users.noreply.github.com>
2022-02-27 22:45:47 -0500

    Update SerialThreadBinary.cs

--- Assets/Scripts/SerialThreadBinary.cs
+++ Assets/Scripts/SerialThreadBinary.cs
@@ -1,8 +1,13 @@
+using System;
 using UnityEngine;
 using System.IO.Ports;
 
 public class SerialThreadBinary : AbstractSerialThread
 {
+    private byte[] buffer = new byte[12];
+    private int state = 0;
+    private int bytesExpect = 0;
+
     public SerialThreadBinary(string portName,
                                        int baudRate,
                                        int delayBeforeReconnecting,
@@ -15,11 +20,38 @@ public class SerialThreadBinary : AbstractSerialThread
 
     protected override void SendToWire(object message, SerialPort serialPort)
     {
-        // Coloca aquí tu código
+        byte[] binaryMessage = (byte[])message;
+        serialPort.Write(binaryMessage, 0, binaryMessage.Length);
     }
 
     protected override object ReadFromWire(SerialPort serialPort)
     {
-        // Coloca aquí tu código
+        byte[] buff = null; 
+        
+        if (state == 0)
+        {
+            if (serialPort.BytesToRead > 0)
+            {
+                state = 1;
+                serialPort.Read(buffer, 0, 1);
+                bytesExpect = Convert.ToInt32(buffer[0]);
+            }
+        }
+
+        if (state == 1)
+        {
+            if (serialPort.BytesToRead > bytesExpect-1)
+            {
+                serialPort.Read(buffer, 0, bytesExpect);
+                byte[] returnBuffer = new byte[bytesExpect];
+                System.Array.Copy(buffer, returnBuffer, bytesExpect);
+                state = 0;
+                buff = returnBuffer;
+                //return returnBuffer;
+            }
+        }
+
+        return buff;
+
     }
 }
